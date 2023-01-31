2c8af0caa573b7343d421e6e0c4556609baa98c7
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-01 00:59:24 +0330

    Feat:add components to App.jsx and create component file with basic struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import Navbar from "./component/Navbar.jsx";
+import Sidebar from "./component/Sidebar.jsx";
+import Hero from "./component/Hero.jsx";
+import Submenu from "./component/Submenu.jsx";
 
 function App() {
 
   return (
-
+    <>
+      <Navbar/>
+      <Sidebar/>
+      <Hero/>
+      <Submenu/>
+    </>
   )
 }
 
---
+++ src/component/Submenu.jsx
@@ -0,0 +1,6 @@
+const Submenu=() =>{
+
+
+}
+
+export default Submenu;
