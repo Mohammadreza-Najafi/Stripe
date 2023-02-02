96b47fab78eea82f1b1fb701d6f508eceb18b57b
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-02 14:48:34 +0330

    focus on hero component

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <>
-      <Navbar/>
-      <Sidebar/>
+      {/*<Navbar/>*/}
+      {/*<Sidebar/>*/}
       <Hero/>
-      <Submenu/>
+      {/*<Submenu/>*/}
     </>
   )
 }
