13c863fa54e0f1fb706069aedb56ea0318276ef7
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-02 14:24:27 +0330

    Feat:complete context, and write some method for handle states.methods: openSidebar, closeSidebar, openSubmenu, closeSubmenu

--- src/context.jsx
+++ src/context.jsx
@@ -10,10 +10,28 @@ const AppProvider=({children}) => {
     const [page,setPage] = useState({page:'',link:[] })
     const [location,setLocation]=useState({})
 
-
+    const openSidebar = () =>{
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () =>{
+        setIsSidebarOpen(false)
+    }
+    const openSubmenu = (text,coordinates) =>{
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page)
+        setLocation(coordinates)
+        setIsSubmenuOpen(true)
+    }
+    const closeSubmenu = () => {
+      setIsSubmenuOpen(false)
+    }
 
     return(
-
+        <AppContext.Provider
+            value={ {isSidebarOpen, openSidebar, closeSidebar, isSubmenuOpen, openSubmenu, page, location} }
+        >
+            {children}
+        </AppContext.Provider>
     )
 }
 
